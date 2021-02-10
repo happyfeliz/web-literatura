c5c2c91782c40d0f63d2221c6e50e52b057974b1
Roberto <75329205+happyfeliz@users.noreply.github.com>
2021-02-09 21:17:40 -0600

    Initial Commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-Document.getElementById("#Home") = _home
+Document.querySelectorAll("#Home") = _home
 Document.getElementById("#Back") = _back
 Document.getElementById("#Profile") = _profile
 
 _home.addEventListener(onclick,homeLoad())
 
 function homeLoad(){
-    alert("Hoola")
+    alert("hola")
 }
