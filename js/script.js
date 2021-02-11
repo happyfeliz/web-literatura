3454535a92217646009f55afc99582fe1919d086
Roberto <75329205+happyfeliz@users.noreply.github.com>
2021-02-11 09:23:18 -0600

    Update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var _home = document.getElementById("Home-button") ;
+var _back = document.getElementById("Back-button") ;
+var _profile = document.getElementById("Profile-button") ;
+
+_profile.addEventListener("click", (e)=>{
+    alert("hola");
+})
+
+console.log(_home);
+console.log(_back);
+console.log(_profile);
+
+alert("hola")
