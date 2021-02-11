149d94656eab8ec57c04629c0404020682fb8c02
Roberto <75329205+happyfeliz@users.noreply.github.com>
2021-02-10 22:57:20 -0600

    Important_Update

--- index.js
+++
@@ -1,9 +0,0 @@
-Document.querySelectorAll("#Home") = _home
-Document.getElementById("#Back") = _back
-Document.getElementById("#Profile") = _profile
-
-_home.addEventListener(onclick,homeLoad())
-
-function homeLoad(){
-    alert("hola")
-}
---
+++ js/index.js
@@ -0,0 +1,11 @@
+_home = document.getElementById("Home-button") ;
+_back = document.getElementById("Back-button") ;
+_profile = document.getElementById("Profile-button") ;
+
+_profile.addEventListener("click", (e)=>{
+    alert("hola");
+})
+
+console.log(_home);
+console.log(_back);
+console.log(_profile);
