36e0956808bf4f596342cae07023330c8d0d7e21
Roberto <75329205+happyfeliz@users.noreply.github.com>
2021-02-12 10:28:44 -0600

    Update

--- js/page_2.js
+++ js/page_2.js
@@ -25,3 +25,15 @@ _profile.addEventListener("click", (e)=>{ ;
     }
 
 })
+
+var _conclusions = document.getElementById("conclusiones");
+var _exit = document.getElementById("exit")
+var _intro= document.getElementById("intro");
+
+_conclusions.addEventListener("click", (e)=>{
+    _intro.style.display="flex";
+})
+
+_intro.addEventListener("click", (e)=>{
+    _intro.style.display="none";
+})
